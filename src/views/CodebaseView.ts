2948fdfd44cf0562eb916f4381932d8bb5344efb
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-04-28 13:15:08 +0530

    readonly

--- src/views/CodebaseView.ts
+++ src/views/CodebaseView.ts
@@ -1,41 +1,48 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export class CodebaseView implements vscode.TreeDataProvider<vscode.TreeItem> {
-    private _onDidChangeTreeData: vscode.EventEmitter<vscode.TreeItem | undefined | null | void> = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
-    readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    vscode.TreeItem | undefined | null | void
+  > = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    vscode.TreeItem | undefined | null | void
+  > = this._onDidChangeTreeData.event;
 
-    constructor() {}
+  constructor() {}
 
-    refresh(): void {
-        this._onDidChangeTreeData.fire();
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+    if (!vscode.workspace.workspaceFolders) {
+      return Promise.resolve([]);
     }
 
-    getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
-        return element;
+    if (!element) {
+      return Promise.resolve(this.getWorkspaceItems());
     }
 
-    getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
-        if (!vscode.workspace.workspaceFolders) {
-            return Promise.resolve([]);
-        }
+    return Promise.resolve([]);
+  }
 
-        if (!element) {
-            return Promise.resolve(this.getWorkspaceItems());
-        }
-
-        return Promise.resolve([]);
+  private getWorkspaceItems(): vscode.TreeItem[] {
+    const items: vscode.TreeItem[] = [];
+    if (vscode.workspace.workspaceFolders) {
+      for (const folder of vscode.workspace.workspaceFolders) {
+        const item = new vscode.TreeItem(
+          folder.name,
+          vscode.TreeItemCollapsibleState.Collapsed
+        );
+        item.resourceUri = folder.uri;
+        item.contextValue = "workspaceFolder";
+        items.push(item);
+      }
     }
-
-    private getWorkspaceItems(): vscode.TreeItem[] {
-        const items: vscode.TreeItem[] = [];
-        if (vscode.workspace.workspaceFolders) {
-            for (const folder of vscode.workspace.workspaceFolders) {
-                const item = new vscode.TreeItem(folder.name, vscode.TreeItemCollapsibleState.Collapsed);
-                item.resourceUri = folder.uri;
-                item.contextValue = 'workspaceFolder';
-                items.push(item);
-            }
-        }
-        return items;
-    }
-} 
+    return items;
+  }
+}
